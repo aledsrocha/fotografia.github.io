ab18d1fda20ae88254d2db2b3a002aeb82ba60e9
Alessandro Rocha <aledsrocha@gmail.com>
2022-08-15 18:09:48 -0300

    criando a interface principal

--- index.php
+++ index.php
@@ -5,6 +5,14 @@
  ?>
 
 
- <div>
- 	<h1>teste</h1>
- </div>
+<div class="principal">
+	<h1>SEJA BEM-VINDO</h1>
+	<h2>SÓ MAIS UM APAIXONADO POR FOTOGRAFIA</h2>
+	<p>Este texto que você está lendo, assim como todo o conteúdo que segue abaixo, pode ser editado por você, personalizando assim este modelo e transformando-o em seu site. Você poderá alterar o título, o menu, acrescentar posts e tudo mais próprio de um site. Se este é o modelo que você mais gostou, feche a página e clique no botão “quero este” para continuar a contratação.</p>
+
+	<h3>UM POUCO SOBRE O FOTÓGRAFO</h3>
+	<p>Você poderá alterar o título, o menu, acrescentar posts e tudo mais próprio de um site. Se este é o modelo que você mais gostou, feche a página e clique no botão “quero este” para continuar a contratação.</p>
+
+	<a href="">CONHEÇA O TRABALHO</a>
+</div>
+
