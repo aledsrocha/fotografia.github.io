754bc24bd4fd733aae9f8f5d87f9b5dab8d5b9cb
Alessandro Rocha <aledsrocha@gmail.com>
2022-08-15 20:25:11 -0300

    criando o footer

--- partials/footer.php
+++ partials/footer.php
@@ -2,21 +2,21 @@
 	<div class="row col-sm-3 footer">
 	
 
-	<h3>PROJETO EM ANDAMENTO</h3>
+	<h3><b>PROJETO EM ANDAMENTO</b></h3>
 	<h4>ATHENAS MODELS</h4>
 	<p>Este texto que você está lendo, assim como todo o conteúdo que segue abaixo, pode ser editado por você, personalizando assim este modelo e transformando-o em seu site.</p>
 	
 
-	<h3>14 RICHARD PHOTOGRAPHY
+	<h2>14 RICHARD PHOTOGRAPHY
 TODOS OS DIREITOS RESERVADOS
-</h3>
+</h2>
 	
 </div>
 
 <div class="row col-sm-3 footer">
 	
 
-	<h3>ÚLTIMO PROJETO FINALIZADO</h3>
+	<h3><b>ÚLTIMO PROJETO FINALIZADO</b></h3>
 	<h4>CASAMENTO FERNANDA E JOÃO</h4>
 	<p>Você poderá alterar o título, o menu, acrescentar posts e tudo mais próprio de um site.</p>
 	
@@ -28,7 +28,7 @@ TODOS OS DIREITOS RESERVADOS
 <div class="row col-sm-3 footer">
 
 
-	<h3>CURIOSIDADE DO DIA</h3>
+	<h3><b>CURIOSIDADE DO DIA</b></h3>
 	<h4>A LUZ NA FOTOGRAFIA</h4>
 	<p>Se este é o modelo que você mais gostou, feche a página e clique no botão "quero este" para continuar a contratação.</p>
 	
@@ -40,7 +40,7 @@ TODOS OS DIREITOS RESERVADOS
 <div class="row col-sm-3 footer">
 	
 
-	<h3>PRESENÇA ON-LINE</h3>
+	<h3><b>PRESENÇA ON-LINE</b></h3>
 	
 	
 
