511608bd02a22c41e1ac2cf42250457e71e796cf
Alessandro Rocha <aledsrocha@gmail.com>
2022-08-22 19:52:41 -0300

    finalizando a pagina sobre o fotografo

--- o-fotografo.php
+++ o-fotografo.php
@@ -3,6 +3,171 @@
 	require_once 'partials/menu.php';
 	
  ?>
+<link rel="stylesheet" type="text/css" href="<?=$base;?>/assents/css/fotografo-info.css">
+ <div class="container fotografo-info">
+ 	<div class="row">
+ 		<div class="col-md-10 fotografo-bio">
+ 			<h3>O FOTÓGRAFO</h3>
+ 			<p>Este texto que você está lendo, assim como todo o conteúdo que segue abaixo, pode ser editado por você, personalizando assim este modelo e transformando-o em seu site. Você poderá alterar o título, o menu, acrescentar posts e tudo mais próprio de um site. Se este é o modelo que você mais gostou, feche a página e clique no botão “quero este” para continuar a contratação.</p>
+ 			
+ 		</div><!--fotografo -->
+
+ 		<div class="col-md-4">
+ 			<img width="350px" src="<?=$base;?>/assents/img/foto1/img_01.jpg">
+ 			<h4>Locações externas</h4>
+ 			<p>Este texto que você está lendo, assim como todo o conteúdo que segue abaixo, pode ser editado por você, personalizando este modelo e transformando-o seu site</p>
+ 			
+ 		</div>
+
+ 		<div class="col-md-4">
+ 			<img width="350px" src="<?=$base;?>/assents/img/foto1/img_02.jpg">
+ 			<h4>Locações externas</h4>
+ 			<p>Você poderá alterar o título, o menu, acrescentar posts e tudo mais próprio de um site</p>
+ 			
+ 		</div>
+ 		
+ 		
+ 		<div class="col-md-4">
+ 			<img width="350px" src="<?=$base;?>/assents/img/foto1/img_03.jpg">
+ 			<h4>Fotografia em estúdio</h4>
+ 			<p>Se este é o modelo que você mais gostou, feche a página e clique no botão “quero este” para continuar a contratação. Você poderá alterar o título, o menu, acrescentar posts e tudo mais próprio de um site.</p>
+ 			
+ 		</div>
+ 		 <div>
+ 	<a href="<?=$base?>/trabalhos.php">VER TODOS OS TRABALHOS</a>
+ </div>
+ 	</div>
+
+
+
+ 	
+ </div>
+
+
+  <div class="container fotografo-info">
+ 	<div class="row">
+ 		<div class="col-md-10 fotografo-bio">
+ 			<h3>O QUE ESTÁ QUENTE NA ÁREA</h3>
+ 			<p>Você poderá alterar o título, o menu, acrescentar posts e tudo mais próprio de um site. Se este é o modelo que você mais gostou, feche a página e clique no botão “quero este” para continuar a contratação.</p>
+ 			
+ 		</div><!--fotografo -->
+
+ 		<div class="col-md-3 img-2">
+ 			<img width="200px" src="<?=$base;?>/assents/img/foto1/img_04.jpg">
+ 			<h4>Editorial Trip On</h4>
+ 			<p>Este texto que você está lendo, assim como todo o conteúdo que segue abaixo, pode ser editado por você.</p>
+ 			 <div>
+ 	<a href="<?=$base?>/trabalhos.php">+</a>
+ </div>
+ 			
+ 		</div>
+
+ 		<div class="col-md-3 img-2">
+ 			<img width= "200" src="<?=$base;?>/assents/img/foto1/img_05.jpg">
+ 			<h4>Lançamento do livro A Colheita</h4>
+ 			<p>Você poderá alterar o título, o menu, acrescentar posts e tudo mais próprio de um site.
+Se este é o modelo que você mais gostou, feche a página e clique no botão “quero este” para continuar a contratação.</p>
+ 			 <div>
+ 	<a href="<?=$base?>/trabalhos.php">+</a>
+ </div>
+ 		</div>
+ 		
+ 		
+ 		<div class="col-md-3">
+ 			
+ 			<h4>De utilidade para você, fotógrafo</h4>
+ 			<p>Este texto que você está lendo, assim como todo o conteúdo que segue abaixo.
+
+Pode ser editado por você, personalizando assim este modelo e transformando-o em seu site.
+
+Você poderá alterar o título, o menu, acrescentar posts e tudo mais próprio de um site.
+
+Se este é o modelo que você mais gostou, feche a página e clique no botão “quero este” para continuar a contratação.</p>
+ 			
+ 		</div>
+
+ 		<div class="col-md-3 img-2">
+ 			<img width= "200" src="<?=$base;?>/assents/img/foto1/img_06.jpg">
+ 			<h4>Fotografia em estúdio</h4>
+ 			<p>Se este é o modelo que você mais gostou, feche a página e clique no botão “quero este” para continuar a contratação. Você poderá alterar o título, o menu, acrescentar posts e tudo mais próprio de um site.</p>
+ 			 <div>
+ 	<a href="<?=$base?>/trabalhos.php">+</a>
+ </div>
+ 			
+ 		</div >
+ 		
+ 	</div>
+
+ </div><br><br>
+
+
+  <div class="container fotografo-info">
+ 	<div class="row">
+ 	 		<div class="col-md-6 img-2">
+ 			<img width="530" src="<?=$base;?>/assents/img/foto1/img_07.jpg">
+ 			
+ 		</div>
+
+ 		<div class="col-md-6 img-2">
+ 			
+ 			<h5>Lançamento da galeria de arte do artista John Louvre</h5>
+ 			<p>Este texto que você está lendo, assim como todo o conteúdo que segue abaixo, pode ser editado por você, personalizando assim este modelo e transformando-o em seu site. Você poderá alterar o título, o menu, acrescentar posts e tudo mais próprio de um site. Se este é o modelo que você mais gostou, feche a página e clique no botão “quero este” para continuar a contratação.</p>
+
+ 			 <div>
+ 	<a href="<?=$base?>/trabalhos.php">+</a>
+ </div>
+ 			
+ 		</div>
+ 		
+
+ 	</div>
+
+ </div>
+
+  <div class="container fotografo-info">
+ 	<div class="row">
+ 		
+
+ 		<div class="col-md-3 img-2">
+ 			<img width="200px" src="<?=$base;?>/assents/img/foto1/img_08.jpg">
+ 			<h4>Inauguração bar Kruggen Beer</h4>
+ 			<p>Este texto que você está lendo, assim como todo o conteúdo que segue abaixo, pode ser editado por você, personalizando assim este modelo e transformando-o em seu site.</p>
+ 		
+ 		</div>
+
+ 		<div class="col-md-3">
+ 			
+ 			<h4>Novidades, sempre</h4>
+ 			<p>Este texto que você está lendo, assim como todo o conteúdo que segue abaixo, pode ser editado por você, personalizando assim este modelo e transformando-o em seu site.
+
+Você poderá alterar o título, o menu, acrescentar posts e tudo mais próprio de um site.
+
+Se este é o modelo que você mais gostou, feche a página e clique no botão “quero este” para continuar a contratação.</p>
+ 			
+ 		</div>
+
+ 		<div class="col-md-3 img-2">
+ 			<img width= "200" src="<?=$base;?>/assents/img/foto1/img_09.jpg">
+ 			<h4>Descobrindo o mundo</h4>
+ 			<p>Se este é o modelo que você mais gostou, feche a página e clique no botão.</p>
+ 			 <div>
+ 	<a href="<?=$base?>/trabalhos.php">+</a>
+ </div>
+ 		</div>
+
+
+ 		<div class="col-md-3 img-2">
+ 			<img width= "200" src="<?=$base;?>/assents/img/foto1/img_10.jpg">
+ 			<h4>Ensaio de moda</h4>
+ 			<p>Este texto que você está lendo, assim como todo o conteúdo que segue abaixo, pode ser editado por você.</p>
+ 			 <div>
+ 	<a href="<?=$base?>/trabalhos.php">+</a>
+ </div>
+ 		</div >
+ 		
+ 	</div>
+
+
 
 
  <?php 
