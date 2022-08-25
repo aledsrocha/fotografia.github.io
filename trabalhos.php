43555044f5b2b53e67847a4ac0055d6777e4616a
Alessandro Rocha <aledsrocha@gmail.com>
2022-08-25 18:23:17 -0300

    finalizando a pagina de trabalho

---
+++ trabalhos.php
@@ -0,0 +1,85 @@
+<?php 
+	require_once 'config.php';
+	require_once 'partials/menu.php';
+	
+ ?>
+
+<link rel="stylesheet" type="text/css" href="<?=$base;?>/assents/css/fotografo-info.css">
+
+	<div class="container fotografo-info">
+		<div class="row">
+			<div class="col-md-4">
+				<img width="350px" src="<?=$base;?>/assents/img/jobs/1.jpg">
+ 			<h4>VIAGEM 01</h4>
+ 			<p>Este texto que você está lendo, assim como todo o conteúdo que segue abaixo, pode ser editado por você, personalizando assim este modelo e transformando-o em seu site. Você poderá...</p>
+
+ 		 <div>
+ 	<a href="<?=$base?>/trabalhos.php">+</a>
+ </div>
+				
+			</div>
+
+			<div class="col-md-4">
+				<img width="350px" src="<?=$base;?>/assents/img/jobs/2.jpg">
+ 			<h4>VIAGEM 02</h4>
+ 			<p>Este texto que você está lendo, assim como todo o conteúdo que segue abaixo, pode ser editado por você, personalizando assim este modelo e transformando-o em seu site. Você poderá...</p>
+
+ 		 <div>
+ 	<a href="<?=$base?>/trabalhos.php">+</a>
+ </div>
+				
+			</div>
+
+			<div class="col-md-4">
+				<img width="350px" src="<?=$base;?>/assents/img/jobs/3.jpg">
+ 			<h4>VIAGEM 03</h4>
+ 			<p>Este texto que você está lendo, assim como todo o conteúdo que segue abaixo, pode ser editado por você, personalizando assim este modelo e transformando-o em seu site. Você poderá...</p>
+
+ 		 <div>
+ 	<a href="<?=$base?>/trabalhos.php">+</a>
+ </div>
+				
+			</div><br><br>
+
+
+<div class="col-md-4">
+				<img width="350px" src="<?=$base;?>/assents/img/jobs/4.jpg">
+ 			<h4>EVENTO 06</h4>
+ 			<p>Este texto que você está lendo, assim como todo o conteúdo que segue abaixo, pode ser editado por você, personalizando assim este modelo e transformando-o em seu site. Você poderá...</p>
+
+ 		 <div>
+ 	<a href="<?=$base?>/trabalhos.php">+</a>
+ </div>
+				
+			</div>
+
+			<div class="col-md-4">
+				<img width="350px" src="<?=$base;?>/assents/img/jobs/5.jpg">
+ 			<h4>EVENTO 07</h4>
+ 			<p>Este texto que você está lendo, assim como todo o conteúdo que segue abaixo, pode ser editado por você, personalizando assim este modelo e transformando-o em seu site. Você poderá...</p>
+
+ 		 <div>
+ 	<a href="<?=$base?>/trabalhos.php">+</a>
+ </div>
+				
+			</div>
+
+			<div class="col-md-4">
+				<img width="350px" src="<?=$base;?>/assents/img/jobs/6.jpg">
+ 			<h4>EVENTO 08</h4>
+ 			<p>Este texto que você está lendo, assim como todo o conteúdo que segue abaixo, pode ser editado por você, personalizando assim este modelo e transformando-o em seu site. Você poderá...</p>
+
+ 		 <div>
+ 	<a href="<?=$base?>/trabalhos.php">+</a>
+ </div>
+				
+			</div> <br> <br>
+
+			
+		</div><!--row -->
+		
+	</div><!--container -->
+
+  <?php 
+	require_once 'partials/footer.php';
+ ?>
